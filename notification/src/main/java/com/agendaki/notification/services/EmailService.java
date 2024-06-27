f216e9e0763ce7694d49949e2e39414d1d107615
guilhermevini2013 <guilhermevini2013@hotmail.com>
2024-06-27 10:57:57 -0300

    feat: configuration the properties of email sender in application.properties

---
+++ notification/src/main/java/com/agendaki/notification/services/EmailService.java
@@ -0,0 +1,6 @@
+package com.agendaki.notification.services;
+
+public interface EmailService {
+
+    void sendEmail();
+}
---
+++ notification/src/main/java/com/agendaki/notification/services/EmailServiceImpl.java
@@ -0,0 +1,18 @@
+package com.agendaki.notification.services;
+
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    private final JavaMailSender mailSender;
+
+    public EmailServiceImpl(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    @Override
+    public void sendEmail() {
+
+    }
+}
