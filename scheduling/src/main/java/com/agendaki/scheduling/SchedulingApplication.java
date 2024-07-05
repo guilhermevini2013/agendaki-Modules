e21f26ecc46f97d07e9caf7ed0e7247a90e1422b
guilhermevini2013 <guilhermevini2013@hotmail.com>
2024-07-05 13:09:23 -0300

    refactor: add properties of data base in application.properties

--- scheduling/src/main/java/com/agendaki/scheduling/SchedulingApplication.java
+++ scheduling/src/main/java/com/agendaki/scheduling/SchedulingApplication.java
@@ -2,8 +2,10 @@ package com.agendaki.scheduling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SchedulingApplication {
 
     public static void main(String[] args) {
