b170c4a62aa50159b2233c5609befcf12591b120
MurilloFornazzaCosta <mfornazzamurilloo@gmail.com>
2024-08-13 16:57:34 -0300

    feat: implement the login page

--- front-end/src/app/header-nav-bar/header-nav-bar.component.ts
+++ front-end/src/app/header-nav-bar/header-nav-bar.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header-nav-bar',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header-nav-bar.component.html',
   styleUrl: './header-nav-bar.component.css'
 })
---
+++ front-end/src/app/register-page/register-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-register-page',
+  standalone: true,
+  imports: [MatIcon],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.css'
+})
+export class RegisterPageComponent {
+
+}
