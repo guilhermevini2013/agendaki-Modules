4291de886bb2ac25d0dbfd6f260a49683fd526b7
Guilhermevini2013 <guilhermevini2013@hotmail.com>
2024-08-13 21:08:57 -0300

    feat: finish the form of login page

--- front-end/src/app/register-page/register-page.component.ts
+++ front-end/src/app/register-page/register-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
+import {FormRegisterComponent} from "./form-register/form-register.component";
 
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [MatIcon],
+  imports: [MatIcon, FormRegisterComponent],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
