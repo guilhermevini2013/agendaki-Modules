8e3e527741e28447bd1ea66c217e3155e21dcd39
MurilloFornazzaCosta <mfornazzamurilloo@gmail.com>
2024-08-15 16:20:02 -0300

    feat: implement login page

---
+++ administration-frontEnd/src/app/components/login-page/login-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import { FormLoginComponent } from './form-login/form-login.component';
+@Component({
+  selector: 'app-login-page',
+  standalone: true,
+  imports: [MatIconModule,FormLoginComponent],
+  templateUrl: './login-page.component.html',
+  styleUrl: './login-page.component.css'
+})
+export class LoginPageComponent {
+
+}
