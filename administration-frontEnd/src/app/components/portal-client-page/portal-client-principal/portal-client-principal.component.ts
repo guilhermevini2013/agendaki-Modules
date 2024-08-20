9f410cc08cc796722d2ebae5c559fdcd00afe0a2
MurilloFornazzaCosta <mfornazzamurilloo@gmail.com>
2024-08-20 16:46:17 -0300

    feat: implement navigation bar in portal client page

--- administration-frontEnd/src/app/components/portal-client-page/portal-client-principal/portal-client-principal.component.ts
+++ administration-frontEnd/src/app/components/portal-client-page/portal-client-principal/portal-client-principal.component.ts
@@ -1,20 +1,30 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import { MediaMatcher } from '@angular/cdk/layout';
+import { NgFor, NgIf } from '@angular/common';
+import { ChangeDetectorRef, Component} from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatListModule} from '@angular/material/list';
+import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-portal-client-principal',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule,MatToolbarModule,MatIcon,NgFor,NgIf,MatListModule,RouterOutlet],
   templateUrl: './portal-client-principal.component.html',
   styleUrl: './portal-client-principal.component.css'
 })
-export class PortalClientPrincipalComponent implements OnInit{
+export class PortalClientPrincipalComponent{
+  
+  
 
-  constructor(private router:Router) {
+  constructor() {
+
   }
 
-  ngOnInit(): void {
+  ngOnDestroy(): void {
+  
   }
-
 
 }
