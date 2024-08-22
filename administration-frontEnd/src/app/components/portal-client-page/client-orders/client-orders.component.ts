843b0670be5050509bfd7028857f39bea6719e0e
MurilloFornazzaCosta <mfornazzamurilloo@gmail.com>
2024-08-22 16:46:42 -0300

    feat: client-order

--- administration-frontEnd/src/app/components/portal-client-page/client-orders/client-orders.component.ts
+++ administration-frontEnd/src/app/components/portal-client-page/client-orders/client-orders.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-client-orders',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './client-orders.component.html',
   styleUrl: './client-orders.component.css'
 })
