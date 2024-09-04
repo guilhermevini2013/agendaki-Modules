9985f4c79488677fefe4cc075418eed910ae07c3
SENAI Zerbini <aluno@escolar509.sp.senai.br>
2024-09-04 16:42:21 -0300

    feat: implement dragAndDrop of pre-visualizer0

--- administration-frontEnd/src/app/components/administration-page/agendaki-tools/pre-visualizer/pre-visualizer.component.ts
+++ administration-frontEnd/src/app/components/administration-page/agendaki-tools/pre-visualizer/pre-visualizer.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-
+import { Component, Type } from '@angular/core';
+import { CdkDropList, CdkDrag, CdkDragPlaceholder, moveItemInArray, CdkDragDrop } from '@angular/cdk/drag-drop';
+import { CommonModule } from '@angular/common';
+import { InputComponent } from '../sections/input/input.component';
+export interface ComponentWithId {
+  id: string;
+  component: Type<any>;
+}
 @Component({
   selector: 'app-pre-visualizer',
   standalone: true,
-  imports: [],
+  imports: [
+    CdkDropList,
+    CdkDrag,
+    CdkDragPlaceholder,
+    CommonModule, 
+  ],
   templateUrl: './pre-visualizer.component.html',
-  styleUrl: './pre-visualizer.component.css'
+  styleUrls: ['./pre-visualizer.component.css'] // Corrigido de styleUrl para styleUrls
 })
 export class PreVisualizerComponent {
+  components: ComponentWithId[] = [
+    { id: 'input', component: InputComponent },
+    { id: 'input2', component: InputComponent },
+  ];
 
+  drop(event: CdkDragDrop<Type<any>[]>) {
+    moveItemInArray(this.components, event.previousIndex, event.currentIndex);
+    console.log(this.components);
+    
+  }
 }
---
+++ administration-frontEnd/src/app/components/administration-page/agendaki-tools/sections/input/input.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-input',
+  standalone: true,
+  imports: [],
+  templateUrl: './input.component.html',
+  styleUrl: './input.component.css'
+})
+export class InputComponent {
+  public label:string="E-mail";
+  public placeHolder:string="email";
+}
