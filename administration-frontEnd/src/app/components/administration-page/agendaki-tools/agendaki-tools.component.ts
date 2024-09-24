f316884ee8ae67675118fb8f7e7f1574bef0ae4f
Emma <emmanuel.hrb@gmail.com>
2024-09-24 17:11:33 -0300

    feat: android-sizing

--- administration-frontEnd/src/app/components/administration-page/agendaki-tools/agendaki-tools.component.ts
+++ administration-frontEnd/src/app/components/administration-page/agendaki-tools/agendaki-tools.component.ts
@@ -1,11 +1,14 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, HostListener, ViewChild} from '@angular/core';
 import {PreVisualizerComponent} from './pre-visualizer/pre-visualizer.component';
 import {AddComponentComponent} from './add-component/add-component.component';
+import { NgIf } from '@angular/common';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
+import { MatButton, MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-agendaki-tools',
   standalone: true,
-  imports: [PreVisualizerComponent, AddComponentComponent],
+  imports: [PreVisualizerComponent, AddComponentComponent, NgIf, MatIconModule, MatButtonModule],
   templateUrl: './agendaki-tools.component.html',
   styleUrl: './agendaki-tools.component.css'
 })
@@ -14,5 +17,20 @@ export class AgendakiToolsComponent {
 
   getJsonForTemplateByPreVisualizer() {
     return this.preVisualizerComponent.getJsonForTemplate();
+  }
+
+  screenSize: number = 0;
+
+  constructor() {
+    this.checkScreenSize(window.innerWidth);
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.checkScreenSize((event.target as Window).innerWidth);
+  }
+
+  checkScreenSize(width: number) {
+    this.screenSize = width;
   }
 }
